e9bac6461df668b70cffd29f6abc1a960c9e9a3a
Alexander Babenko <asrbbnk@gmail.com>
2023-04-19 17:30:25 +0300

    Concurrency. Print in order (easy)

---
+++ src/main/java/leetcode/problems/concurrency/PrintInOrder.java
@@ -0,0 +1,34 @@
+package leetcode.problems.concurrency;
+
+import java.util.concurrent.Semaphore;
+
+// @see <a href="https://leetcode.com/problems/print-in-order/">1114. Print in Order</a>
+
+public class PrintInOrder {
+
+    private final Semaphore sem1, sem2;
+
+    public PrintInOrder() {
+        sem1 = new Semaphore(0);
+        sem2 = new Semaphore(0);
+    }
+
+    public void first(Runnable printFirst) throws InterruptedException {
+        // printFirst.run() outputs "first". Do not change or remove this line.
+        printFirst.run();
+        sem1.release();
+    }
+
+    public void second(Runnable printSecond) throws InterruptedException {
+        // printSecond.run() outputs "second". Do not change or remove this line.
+        sem1.acquire();
+        printSecond.run();
+        sem2.release();
+    }
+
+    public void third(Runnable printThird) throws InterruptedException {
+        // printThird.run() outputs "third". Do not change or remove this line.
+        sem2.acquire();
+        printThird.run();
+    }
+}
